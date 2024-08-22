8d9a19b9319ac5336cd514f2651e7d45432a34ae
OmJayvantDevkar <devkarom5@gmail.com>
2024-08-22 18:36:59 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,51 @@
+/* Arthmatic operators */
+/*class Operators
+{
+    public static void main(String[] args) {
+        int a=6,b=4;
+        System.out.println("addition is  "+(a+b));
+        System.out.println("substraction is  "+(a-b));
+        System.out.println("multiplication is  "+(a*b));
+        System.out.println("devision is  "+(a/b));
+    }
+}
+    */
+
+/*Relational operator */
+/*class Operators
+{
+    public static void main(String[] args) {
+        int a=6,b=4;
+        System.out.println(a+">"+b+"=" +(a>b));
+        System.out.println(a+"<"+b+"="+(a<b));
+        System.out.println(a+"=="+b+"="+(a==b));
+        System.out.println(a+">="+b+"="+(a>=b));
+        System.out.println(a+"<="+b+"="+(a<=b));
+
+    }
+}
+    */
+
+// Assigement operator
+/*class Operators{
+    public static void main(String[] args) {
+        int a=6,b=4;
+        System.out.println(a+"+="+b+"=" +(a+=b));
+        System.out.println(a+"-="+b+"="+(a-=b));
+        System.out.println(a+"*="+b+"="+(a*=b));
+        System.out.println(a+"/="+b+"="+(a/=b));
+
+    }
+}
+*/
+
+// logical operators
+class Operators{
+    public static void main(String[] args) {
+        int a=6;
+        int b=4;
+        System.out.println(a+"&&"+b+"=" +(a&&b));
+        System.out.println(a+"//"+b+"="+(a//b));
+
+    }
+}
