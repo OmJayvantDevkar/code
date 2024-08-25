69ed90fdafa9dd0b8ecfd90b2fdb554fd8762d4c
OmJayvantDevkar <devkarom5@gmail.com>
2024-08-25 18:55:04 +0530

    Create Input.java
    Taking values from user using Scanner class

---
+++ Java/Input.java
@@ -0,0 +1,140 @@
+import java.util.*;
+//printing string using scanner class
+/*class Input {
+    public static void main(String[] args) {
+         
+         Scanner s1=new Scanner(System.in);
+         String name=s1.next();
+         System.out.println(name);
+        
+    }
+    
+}
+*/
+
+//addtion program using scanner class
+/*class Input{
+public static void main(String[] args)
+ {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int sum=a+b;
+    System.out.println(sum);
+}
+}
+*/
+
+// addition of 2 numbers (takig values from user)
+/*class Input{
+    public static void main(String[] args)
+     {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first no:");
+        int a=sc.nextInt();
+        System.out.println("enter the second no:");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("sum of a and b is "+sum);
+    }
+    }*/
+
+    // program to check wheather you are eligible for voting or not (taking values from user)
+   /* class Input{
+        public static void main(String[] args) {
+            {   
+                Scanner sc=new Scanner(System.in);
+                System.out.println("enter your age:");
+                int age=sc.nextInt();
+                
+                if(age>=18)
+                {
+                    System.out.println("eligilable for voting...");
+                }
+                else
+                {
+                    System.out.println("not eligible for voting");
+                }
+
+            }
+        }
+    }
+    */
+
+// program to find the give number is even or odd (taking a input from user)
+/*class Input{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your number:");
+        int number=sc.nextInt();
+
+        if(number%2==0)
+        {
+            System.out.println("num is even");       
+        }
+        else{
+            System.out.println("num is odd");
+        }
+
+    }
+
+}
+    */
+
+
+//
+/*class Input
+{
+    public static void main(String[] args) 
+    {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your no:");
+    int a=sc.nextInt();
+    System.out.println("enter your 2nd no:");
+    int b=sc.nextInt();
+
+
+    if(a==b)
+    {
+        System.out.println("a and b is equal");
+    }
+        else if(a>b)
+        {
+            System.out.println("a is greater than b ");
+        }
+    else
+        {
+        System.out.println("b is greater than a");
+        }
+    }
+    }
+*/
+
+//
+
+/*class Input{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter button which you want:");
+        int button=sc.nextInt();
+
+        if(button==1)
+        {
+            System.out.println("hello");
+        }
+            else if(button==2)
+            {
+                System.out.println("namaste");
+            }
+            else if(button==3)
+            {
+                System.out.println("bongur");
+            }
+         else
+         {
+                System.out.println("input is invalid...");
+                System.out.println("enter the valid statement\n");
+           }
+        }
+    }
+*/
